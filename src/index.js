3695e64c4a9e5da00127833af734a7e4641b9b26
ga25mec <ga25mec@forstudents.onmicrosoft.com>
2018-04-06 19:24:40 +0200

    added next()

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res, next) {
   res.send('Hello World');
+  next();
 })
 
 //app.post('/releases', releaseHandlers.post);
 app.post('/errors', function (req, res, next)  {
   console.log(req.body);
   res.status(400).send();
+  next();
 });
  
 app.listen(3000, () => console.log('Server listening on port 3000'));
